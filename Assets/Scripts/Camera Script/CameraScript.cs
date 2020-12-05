82277d238bd1999afb6d86f0b11ddb1c55cf8c12
xcboi2k <darkspikyshark@gmail.com>
2020-12-05 11:07:54 +0800

    Version 2.0.0. Update 12-05-2020

--- Assets/Scripts/Camera Script/CameraScript.cs
+++ Assets/Scripts/Camera Script/CameraScript.cs
@@ -5,25 +5,64 @@ using UnityEngine;
 public class CameraScript : MonoBehaviour
 {
     public static float offsetX;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
+    private const string selectedShip = "Selected Ship";
+
     void Update()
     {
-        if (PlayerScript.instance != null){
-            if(PlayerScript.instance.isAlive){
-                MoveCamera();
-            }
+        int getShip = PlayerPrefs.GetInt(selectedShip);
+        switch(getShip){
+            case 1://X-Wing
+                if (XWingScript.instance != null){
+                    if(XWingScript.instance.isAlive){
+                    MoveCamera();
+                    }
+                }
+                break;
+            case 2://A-Wing
+                if (AWingScript.instance != null){
+                    if(AWingScript.instance.isAlive){
+                    MoveCamera();
+                    }
+                }
+                break;
+            case 3://Y-WIng
+                if (YWingScript.instance != null){
+                    if(YWingScript.instance.isAlive){
+                    MoveCamera();
+                    }
+                }
+                break;
+            case 4://MAAT
+                if (MAATScript.instance != null){
+                    if(MAATScript.instance.isAlive){
+                    MoveCamera();
+                    }
+                }
+                break;
         }
+        
     }
 
     void MoveCamera(){
         Vector3 temp  = transform.position;
-        temp.x = PlayerScript.instance.GetPositionX() + offsetX;
+
+        int getShip = PlayerPrefs.GetInt(selectedShip);
+        switch(getShip){
+            case 1://X-Wing
+                temp.x = XWingScript.instance.GetPositionX() + offsetX;
+                break;
+            case 2://A-Wing
+                temp.x = AWingScript.instance.GetPositionX() + offsetX;
+                break;
+            case 3://Y-WIng
+                temp.x = YWingScript.instance.GetPositionX() + offsetX;
+                break;
+            case 4://MAAT
+                temp.x = MAATScript.instance.GetPositionX() + offsetX;
+                break;
+        }
+
         transform.position = temp;
     }
 }
